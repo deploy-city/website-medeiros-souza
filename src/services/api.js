be3858584d5644a97da82816a599b991cc30b2fc
Guilherme Cabo <guilherme.pcabo@gmail.com>
2020-05-04 15:43:36 -0300

    Fixed production api address

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:8000";
+export const baseURL = "http://api-medeiros-souza.herokuapp.com";
 
 const api = axios.create({
   baseURL: `${baseURL}/api`,
