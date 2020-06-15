025ecb1bd03c91d0ea01f79b34a804cddc041fe1
Guilherme Cabo <deploy@cityconnect.com.br>
2020-06-15 09:18:45 -0300

    Improved displaying of clients

--- src/pages/Home/components/Clients/index.js
+++ src/pages/Home/components/Clients/index.js
@@ -21,59 +21,57 @@ function Clients() {
       </SideTag>
       <ul>
         <li>
-          <div>
-            <img src={dayOne} alt="Day 1" />
-            <p>
-              <b>Day 1 Photographs</b> {t("DayOneText")}
-            </p>
+          <img src={dayOne} alt="Day 1" />
+          <p>
+            <b>Day 1 Photographs</b> {t("DayOneText")}
+          </p>
 
-            <a
-              href="https://www.day1photographs.com/bio"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {t("More About")} Day 1 Photographs <MdArrowForward size={24} />
-            </a>
-          </div>
-          <div>
-            <img src={cityconnect} alt="City Connect" />
-            <p>
-              <b>City Connect</b> {t("CityConnectText")}
-            </p>
+          <a
+            href="https://www.day1photographs.com/bio"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {t("More About")} Day 1 Photographs <MdArrowForward size={24} />
+          </a>
+        </li>
+        <li>
+          <img src={cityconnect} alt="City Connect" />
+          <p>
+            <b>City Connect</b> {t("CityConnectText")}
+          </p>
 
-            <a
-              href="https://new.cityconnect.com.br"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {t("More About")} City Connect <MdArrowForward size={24} />
-            </a>
-          </div>
-          <div>
-            <img src={raise} alt="Raise Investments" />
-            <p>
-              <b>Raise Investments</b> {t("RaiseInvestmentsText")}
-            </p>
+          <a
+            href="https://new.cityconnect.com.br"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {t("More About")} City Connect <MdArrowForward size={24} />
+          </a>
+        </li>
+        <li>
+          <img src={raise} alt="Raise Investments" />
+          <p>
+            <b>Raise Investments</b> {t("RaiseInvestmentsText")}
+          </p>
 
-            <a
-              href="https://raise.investments/about-us/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {t("More About")} Raise Investments <MdArrowForward size={24} />
-            </a>
-          </div>
-          <div>
-            <img src={rps} alt="RPS" />
-            <p>
-              <b>Restoration Precare Solutions</b>{" "}
-              {t("RestorationPrecareSolutionsText")}
-            </p>
+          <a
+            href="https://raise.investments/about-us/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {t("More About")} Raise Investments <MdArrowForward size={24} />
+          </a>
+        </li>
+        <li>
+          <img src={rps} alt="RPS" />
+          <p>
+            <b>Restoration Precare Solutions</b>{" "}
+            {t("RestorationPrecareSolutionsText")}
+          </p>
 
-            <a href="">
-              {t("More About")} RPS <MdArrowForward size={24} />
-            </a>
-          </div>
+          <a href="">
+            {t("More About")} RPS <MdArrowForward size={24} />
+          </a>
         </li>
       </ul>
     </Container>
--- src/pages/Home/components/Clients/styles.js
+++ src/pages/Home/components/Clients/styles.js
@@ -22,81 +22,84 @@ export const Container = styled.div`
     width: 100%;
     list-style-type: none;
     display: flex;
-    flex-direction: column;
+    flex-direction: row;
+    justify-content: space-evenly;
+    padding: 20px;
+
+    @media (max-width: 768px) {
+      flex-direction: column;
+      align-items: center;
+    }
 
     li {
-      display: flex;
-      flex-direction: row;
-      justify-content: space-evenly;
+      background: #fff;
+      height: 380px;
+      width: 400px;
+      padding: 30px 10px 0;
 
-      @media (max-width: 640px) {
-        flex-direction: column;
-        align-items: center;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+
+      -webkit-box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.75);
+      -moz-box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.75);
+      box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.75);
+
+      @media (min-width: 768px) {
+        & + li {
+          margin-left: 20px;
+        }
       }
 
-      div {
-        background: #fff;
-        height: 380px;
-        width: 400px;
-        max-width: 90vw;
-        padding: 30px 10px 0;
+      @media (max-width: 768px) {
+        width: 100%;
 
+        & + li {
+          margin-top: 20px;
+        }
+      }
+
+      img {
+        height: 100px;
+        max-width: 80%;
+        object-fit: contain;
+      }
+
+      p {
+        height: 100px;
+        margin-top: 40px;
+        max-width: 90%;
+        padding: 0 30px;
+        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+          Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+        font-size: 18px;
+        color: #707070;
+
+        @media (max-width: 640px) {
+          font-size: 16px;
+        }
+      }
+
+      a {
         display: flex;
-        flex-direction: column;
         align-items: center;
+        text-decoration: none;
+        color: #ff0037;
+        font-weight: bold;
+        margin-top: 40px;
+        font-size: 20px;
+        transition: 0.2s;
 
-        -webkit-box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.75);
-        -moz-box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.75);
-        box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.75);
-
-        @media (max-width: 768px) {
-          & + div {
-            margin-top: 10px;
-          }
-        }
-
-        img {
-          height: 100px;
-          max-width: 80%;
-          object-fit: contain;
-        }
-
-        p {
-          height: 100px;
-          margin-top: 40px;
-          max-width: 90%;
-          padding: 0 30px;
-          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
-            Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+        @media (max-width: 640px) {
           font-size: 18px;
-          color: #707070;
-
-          @media (max-width: 640px) {
-            font-size: 16px;
-          }
         }
 
-        a {
-          display: flex;
-          align-items: center;
-          text-decoration: none;
-          color: #ff0037;
-          font-weight: bold;
-          margin-top: 40px;
-          font-size: 20px;
-          transition: 0.2s;
+        &:hover {
+          color: ${shade(0.3, "#ff0037")};
+        }
 
-          @media (max-width: 640px) {
-            font-size: 18px;
-          }
-
-          &:hover {
-            color: ${shade(0.3, "#ff0037")};
-          }
-
-          svg {
-            margin-left: 10px;
-          }
+        svg {
+          margin-left: 10px;
         }
       }
     }
